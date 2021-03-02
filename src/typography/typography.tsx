e9cbeb5ba8a31797a0465e618899f28859c666a9
minnyww <minrrverse@gmail.com>
2021-03-02 23:07:00 +0700

    [update]

--- src/typography/typography.tsx
+++ src/typography/typography.tsx
@@ -1,65 +1,86 @@
 import Text from 'antd/lib/typography/Text'
 import TextParagraph from 'antd/lib/typography/Paragraph'
-import * as React from 'react'
-import styled from 'styled-components'
+import TextLink from 'antd/lib/typography/Link'
+import Title from 'antd/lib/typography/Title'
+import React from 'react'
+import styled, { css } from 'styled-components'
 
-type TypographyProps = {
-  strong?: boolean
-  style?: any
-}
+const getColor = css`
+  color: ${({ color }: any) => color && `${color} !important`};
+`
 
-const HeadlineText = styled(Text)`
+const getColorWithDefaultColor = css`
+  color: ${({ color }: any) =>
+    color ? `${color} !important` : `var(--gray-400)`};
+`
+
+const getFontWeight = css`
+  font-weight: ${({ strong }: any) => (strong ? 700 : 400)};
+`
+
+const HeadlineText = styled(Title)<any>`
   font-size: var(--font-24) !important;
   font-weight: 700;
+  ${getColor}
 `
 
-const SubHeadlineText = styled(Text)`
+const SubHeadlineText = styled(TextParagraph)<any>`
   font-size: var(--font-18) !important;
-  font-weight: ${({ strong }) => (strong ? 700 : 400)};
+  ${getFontWeight};
+  ${getColor}
 `
 
-const ParagraphText = styled(TextParagraph)`
+const ParagraphText = styled(TextParagraph)<any>`
   font-size: var(--font-16) !important;
-  color: var(--gray-400);
-  font-weight: ${({ strong }) => (strong ? 700 : 400)};
+  ${getColorWithDefaultColor}
+  ${getFontWeight};
 `
 
-const DescriptionText = styled(Text)`
+const DescriptionText = styled(TextParagraph)<any>`
   font-size: var(--font-14) !important;
-  color: var(--gray-400);
-  font-weight: ${({ strong }) => (strong ? 700 : 400)};
+  ${getColorWithDefaultColor};
+  ${getFontWeight};
 `
 
-const SmallText = styled(Text)`
+const SmallText = styled(Text)<any>`
   font-size: var(--font-12) !important;
-  color: var(--gray-400);
-  font-weight: ${({ strong }) => (strong ? 700 : 400)};
+  ${getColorWithDefaultColor};
+  ${getFontWeight};
 `
 
-const Headline: React.FC<TypographyProps> = (props) => {
+const LinkText = styled(TextLink)<any>``
+
+const Headline: React.FC<any> = (props) => {
   return <HeadlineText {...props} />
 }
 
-const SubHeadline: React.FC<TypographyProps> = (props) => {
+const SubHeadline: React.FC<any> = (props) => {
   return <SubHeadlineText {...props} />
 }
 
-const Paragraph: React.FC<TypographyProps> = (props) => {
+const Paragraph: React.FC<any> = (props) => {
   return <ParagraphText {...props} />
 }
 
-const Description: React.FC<TypographyProps> = (props) => {
+const Description: React.FC<any> = (props) => {
   return <DescriptionText {...props} />
 }
 
-const Small: React.FC<TypographyProps> = (props) => {
+const Small: React.FC<any> = (props) => {
   return <SmallText {...props} />
 }
 
-export default {
+const Link: React.FC<any> = (props) => {
+  return <LinkText {...props} />
+}
+
+const Typography = {
   Headline,
   SubHeadline,
   Paragraph,
   Description,
-  Small
+  Small,
+  Link
 }
+
+export default Typography
